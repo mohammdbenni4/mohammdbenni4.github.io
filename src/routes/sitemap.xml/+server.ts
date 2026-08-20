93ef75c00d63d03eb483ebdabeda645046665915
Mohammd Benni <mohammdbenni4@gmail.com>
2026-08-20 13:21:43 +0300

    sitemap fix

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -23,7 +23,7 @@ export const GET: RequestHandler = async ({ setHeaders }) => {
 <urlset
 	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
 	xmlns:xhtml="http://www.w3.org/1999/xhtml"
-	xmlns:image="http://www.w3.org/1999/image/1.1"
+	xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
 >
 	<url>
 		<loc>${meta.url}/</loc>
